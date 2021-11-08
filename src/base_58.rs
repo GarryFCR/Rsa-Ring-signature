0188a9c888e807045ca1edaf99c55fc37b411855
GarryFCR <garryranee@gmail.com>
2021-11-08 17:42:55 +0530

    Base_58 encoding added

--- src/base_58.rs
+++ src/base_58.rs
@@ -1,9 +1,22 @@
-pub fn base_58(buf: &mut [u8]) {
-    //const BASE58_TABLE: &str = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
+// Encode a byte array into a base-58 encoded string
 
-    let num = as_u32_be(buf);
+pub fn base_58(buf: &mut [u8]) -> String {
+    let table = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
+    let base_table: Vec<char> = table.chars().collect();
 
-    println!("{}", num);
+    let mut num_be = as_u32_be(buf);
+
+    let base: u32 = 58;
+    let mut index: usize;
+    let mut code = String::from("");
+
+    while num_be != 0 {
+        index = (num_be % base) as usize;
+        num_be = num_be / base;
+        let temp = base_table[index].to_string();
+        code = temp + &code;
+    }
+    return code;
 }
 
 fn as_u32_be(array: &mut [u8]) -> u32 {
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,6 @@ mod base_58;
 
 fn main() {
     let mut numbers: [u8; 4] = [1, 2, 3, 4];
-    println!("Hello, world!");
-    base_58::base_58(&mut numbers);
+    let code = base_58::base_58(&mut numbers);
+    println!("{}", code);
 }
