be1392820a151352b85fc612fa96f3c043364201
GarryFCR <garryranee@gmail.com>
2021-11-13 21:33:56 +0530

    Encrypt and decrypt

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ mod rsa_ring;
 
 //mod rsa_ring;
 //use rand::rngs::OsRng;
-use rsa::RsaPublicKey;
+use rsa::{BigUint, RsaPublicKey};
 
 fn main() {
     /*
@@ -40,8 +40,9 @@ fn main() {
         //-----------------------------------------------------------------------------------
 
     */
+
+    //key generate
     let list = rsa_ring::generate_keys(512, 2);
-    // println!("{:?}", list);
 
     let e = list[0].clone();
     let temp = list[0].clone();
@@ -51,12 +52,31 @@ fn main() {
     for i in list.iter() {
         pub_list.push(RsaPublicKey::from(i));
     }
+
+    //init
     let r = rsa_ring::Rsasign::init(pub_list, e);
+
+    //g()
     r.g();
     println!("\n{:?}\n", temp);
     let hello = String::from("Hello, world!");
+
+    //hash
     rsa_ring::hash(hello);
 
+    //encrypt
     let random_list = rsa_ring::generate_rand(1);
     println!("{:?}", random_list);
+
+    let key = rsa_ring::generate_rand(1);
+    println!("{:?}", key);
+    let text = String::from("Hello world");
+    let m = rsa_ring::encrypt(key[0].clone(), text);
+
+    //decrypt
+    rsa_ring::decrypt(key[0].clone(), m);
+
+    let byte = String::from("Hello world");
+
+    println!("{:?}", BigUint::from_bytes_be(byte.as_bytes()));
 }
--- src/rsa_ring.rs
+++ src/rsa_ring.rs
@@ -1,12 +1,18 @@
+extern crate aes;
+extern crate block_modes;
 extern crate hex;
+extern crate hex_literal;
 extern crate rand;
 extern crate rsa;
 extern crate sha2;
 
+use aes::Aes256;
+use block_modes::block_padding::Pkcs7;
+use block_modes::{BlockMode, Cbc};
+use hex_literal::hex;
 use rand::rngs::OsRng;
 use rsa::{BigUint, PublicKeyParts, RsaPrivateKey, RsaPublicKey};
 use sha2::{Digest, Sha256};
-
 pub struct Rsasign {
     set: Vec<RsaPublicKey>,
     signer: RsaPrivateKey,
@@ -28,19 +34,37 @@ impl Rsasign {
         }
     }
 
-    /* pub fn sign(&self, m: String) {
+    /*pub fn sign(&self, m: String) {
         let key = hash(m);
-        let glue = generate_rand(1);
+        let glue = generate_rand(1)[0];
+
         let mut xi_list: Vec<BigUint> = vec![];
         let mut yi_list: Vec<BigUint> = vec![];
 
+        let index : u8 = 0;
+        let pos : u8 = 0;
         for i in self.set.iter() {
             if *i != RsaPublicKey::from(self.signer.clone()) {
                 let x = generate_rand(1);
                 xi_list.push(x[0].clone());
                 let y = self.g();
+                yi_list.push(y);
+            } else {
+                pos =index;
             }
+            index+=1;
         }
+
+        let mut c : BigUint;
+        let xor = glue.clone();
+        for j in 0..pos{
+           xor = xor.Bit_Xor(yi_list[j].clone());
+            c = encrypt(key, m)
+        }
+
+
+
+
     }*/
 }
 //Generates a list of rsa key pairs
@@ -77,10 +101,34 @@ pub fn generate_rand(n: u8) -> Vec<BigUint> {
     return list;
 }
 
-pub fn encrypt(key: String, m: String) -> BigUint {
-    let mut text = String::from("");
-    text.push_str(&key);
-    text.push_str(&m);
-    let hash_text = hash(text);
-    return hash_text;
+pub fn encrypt(key: BigUint, m: String) -> BigUint {
+    type Aes256Cbc = Cbc<Aes256, Pkcs7>;
+    let key_new = key.to_str_radix(16);
+    let iv = hex!("f0f1f2f3f4f5f6f7f8f9fafbfcfdfeff");
+    let cipher = Aes256Cbc::new_from_slices(&key_new.into_bytes(), &iv).unwrap();
+
+    // buffer must have enough space for message+padding
+    let mut buffer = [0u8; 32];
+    // copy message to the buffer
+    let pos = m.len();
+    buffer[..pos].copy_from_slice(&m.into_bytes());
+
+    let ciphertext = cipher.encrypt(&mut buffer, pos).unwrap();
+    let cipher = BigUint::from_radix_be(ciphertext, 256).unwrap();
+
+    println!("cipher:{:?}", cipher);
+    return cipher;
+}
+
+pub fn decrypt(key: BigUint, m: BigUint) -> BigUint {
+    type Aes256Cbc = Cbc<Aes256, Pkcs7>;
+    let key_new = key.to_str_radix(16);
+    let iv = hex!("f0f1f2f3f4f5f6f7f8f9fafbfcfdfeff");
+    let cipher = Aes256Cbc::new_from_slices(&key_new.into_bytes(), &iv).unwrap();
+
+    let mut buf = m.to_bytes_be();
+    let decrypted_ciphertext = cipher.decrypt(&mut buf).unwrap();
+    let num = BigUint::from_radix_be(decrypted_ciphertext, 256).unwrap();
+    println!("Decipher{:?}", num);
+    return num;
 }
