cf91ca3aea2e7e3769a77127b2a8543920554bca
GarryFCR <garryranee@gmail.com>
2021-11-22 20:04:39 +0530

    Aes256 for blocks of texts

--- src/symmetric.rs
+++ src/symmetric.rs
@@ -1,5 +1,5 @@
 use aes::cipher::{generic_array::GenericArray, BlockDecrypt, BlockEncrypt, NewBlockCipher};
-use aes::{Aes128, Block};
+use aes::{Aes128, Aes256, Block};
 use byteorder::{BigEndian, ReadBytesExt};
 use rsa::BigUint;
 
@@ -41,22 +41,30 @@ pub fn decrypt(key: BigUint, m: BigUint) -> BigUint {
     let num = x.read_u128::<BigEndian>().unwrap();
     return BigUint::from(num);
 }
-/*
-#[allow(dead_code)]
-pub fn encrypt256(key: BigUint, m: BigUint) {
-    let slices = key.to_bytes_be();
-    let mut text = m.to_bytes_be();
-    //println!("Enc:{:?} {:?}", slices.len(), text.len());
-    let key = GenericArray::from_slice(&slices);
-    let texts = GenericArray::from_slice(&text);
-    let block = ParBlocks::from_slice(texts.as_slice());
 
-    let mut block_test = block.clone();
-    // Initialize cipher
-    let cipher = Aes128::new(&key);
-    // Encrypt block in-place
-    cipher.encrypt_par_blocks(&mut block_test);
-    let mut x = block_test.as_slice();
-    println!("{:?}", x);
+#[allow(dead_code)]
+pub fn encrypt256(key: BigUint, m: BigUint) -> BigUint {
+    let slices = key.to_bytes_be();
+    let text = m.to_bytes_be();
+    let no_of_blocks = text.len() / 256;
+    let key = GenericArray::from_slice(&slices);
+
+    let mut cipher_array: Vec<u8> = vec![];
+
+    for i in 0..no_of_blocks {
+        let mut part: Vec<u8> = vec![];
+        for j in 0..8 {
+            part.push(text[(i * 8) + j]);
+        }
+        let block = Block::from_slice(&part);
+        let mut block_test = block.clone();
+
+        // Initialize cipher
+        let cipher = Aes256::new(&key);
+        // Encrypt block in-place
+        cipher.encrypt_block(&mut block_test);
+        cipher_array.extend_from_slice(block_test.as_slice());
+    }
+
+    return BigUint::from_bytes_be(&cipher_array);
 }
-*/
