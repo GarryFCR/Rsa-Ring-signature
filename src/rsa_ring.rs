4b26be6f88234be4377d640fbdc00acbc53164c2
GarryFCR <garryranee@gmail.com>
2021-11-25 00:39:54 +0530

    To do: Fix xi sizes

--- src/rsa_ring.rs
+++ src/rsa_ring.rs
@@ -24,13 +24,12 @@ impl Rsasign {
             signer: e,
         }
     }
-    /*
+    //sign a message
     pub fn sign(&self, m: String) -> (Vec<BigUint>, BigUint) {
         //Choose a key
-        let key = hash(m);
+        let key = hash256(m);
         //Pick a random glue value
-        let temp = BigUint::from(rand::random::<u128>()).to_str_radix(16);
-        let glue = hash(temp);
+        let glue = generate_rand256bytes();
         //Pick random xi ’s and calculate yi's
         let mut xi_list: Vec<BigUint> = vec![];
         let mut yi_list: Vec<BigUint> = vec![];
@@ -78,64 +77,10 @@ impl Rsasign {
         xi_list[pos as usize] = x_s;
         return (xi_list, glue);
     }
-    */
-    pub fn sign(&self, m: String) -> (Vec<BigUint>, BigUint) {
-        //Choose a key
-        let key = hash256(m);
-        //Pick a random glue value
-        let glue = generate_rand256bytes();
-        //Pick random xi ’s and calculate yi's
-        let mut xi_list: Vec<BigUint> = vec![];
-        let mut yi_list: Vec<BigUint> = vec![];
-        let mut index: u8 = 0;
-        let mut pos: u8 = 0;
-        for i in self.set.iter() {
-            if *i != RsaPublicKey::from(self.signer.clone()) {
-                let x = generate_rand();
-                xi_list.push(x.clone());
-                let y = g(x.clone(), i.clone());
-                yi_list.push(y);
-            } else {
-                yi_list.push(BigUint::from_bytes_be(b""));
-                xi_list.push(BigUint::from_bytes_be(b""));
-                pos = index;
-            }
-            index += 1;
-        }
-
-        //Solve C_k,v (y1,y2 , . . . , yr)
-        let mut enc = glue.clone();
-        for j in 0..pos {
-            let c = enc ^ yi_list[j as usize].clone();
-            enc = symmetric::encrypt256bytes(key.clone(), c.clone());
-        }
-
-        let mut v = glue.clone();
-        for j in ((pos + 1)..index).rev() {
-            let dec = symmetric::decrypt256bytes(key.clone(), v);
-            v = dec ^ yi_list[j as usize].clone();
-        }
-
-        //Solve C_k,v (y1,y2 , . . . , yr) = v for ys
-        let mut y_s = symmetric::decrypt256bytes(key.clone(), v);
-        y_s = y_s ^ enc;
-        //g^(-1)
-        let pub_key = RsaPublicKey::from(self.signer.clone());
-        let d = self.signer.d();
-        let n = pub_key.n();
-        let f_r = y_s.clone() % n;
-        let q = y_s.clone() / n;
-        let r = f_r.modpow(d, n);
-
-        let x_s = q * n + r.clone();
-        xi_list[pos as usize] = x_s;
-        return (xi_list, glue);
-    }
 }
 
 //trapdoor function
 fn g(x: BigUint, pub_key: RsaPublicKey) -> BigUint {
-    //let pub_key = RsaPublicKey::from(self.signer.clone());
     let n = pub_key.n();
     let e = pub_key.e();
     let q = x.clone() / n;
@@ -145,17 +90,14 @@ fn g(x: BigUint, pub_key: RsaPublicKey) -> BigUint {
     return gx;
 }
 
-/*pub fn hash(m: String) -> BigUint {
-    let num = blake2_128(&m.into_bytes());
-    return BigUint::from_bytes_be(&num);
-}*/
-
+//generate a random 128 bit
 pub fn generate_rand() -> BigUint {
     let temp = rand::random::<u128>();
     let rand_x = temp >> 1; //so that x is less than 128bits and y is atmost 16 bytes
     return BigUint::from(rand_x);
 }
 
+//verify the ring signature
 pub fn verify(
     pub_key_list: Vec<RsaPublicKey>,
     xi_list: Vec<BigUint>,
@@ -171,7 +113,7 @@ pub fn verify(
     let mut enc = glue.clone();
     for j in 0..xi_list.len() {
         let c = enc ^ yi_list[j as usize].clone();
-        enc = symmetric::encrypt256bytes(key.clone(), c.clone());
+        enc = symmetric::encrypt(key.clone(), c.clone());
     }
 
     if enc == glue {
@@ -180,27 +122,24 @@ pub fn verify(
 
     return false;
 }
-//------------------------------------------------------------------------------------
+
+//using sha256 hash on a message and then using a non-cryptographic hash to get the 128 bit hash
 pub fn hash256(m: String) -> BigUint {
     // create a Sha256 object
     let mut hasher = Sha256::new();
-
     // write input message
     hasher.update(m.into_bytes());
-
     // read hash digest and consume hasher
     let result = hasher.finalize();
     let x = result.as_slice();
 
-    //let num = BigUint::from_bytes_be(x);
-
     let hash128 = blake2_128(x);
     return BigUint::from_bytes_be(&hash128);
 }
+//generate a random 256 bytes
 pub fn generate_rand256bytes() -> BigUint {
     let mut key = [0u8; 256];
     OsRng.fill_bytes(&mut key);
     let rand_num = BigUint::from_bytes_be(&key);
-    //println!("{:?}", rand_num);
     return rand_num;
 }
--- src/symmetric.rs
+++ src/symmetric.rs
@@ -1,57 +1,15 @@
 use aes::cipher::generic_array::GenericArray;
 use aes::cipher::{BlockDecrypt, BlockEncrypt, NewBlockCipher};
 use aes::{Aes128, Block};
-//use byteorder::{BigEndian, ReadBytesExt};
 use rsa::BigUint;
 
-/*
 #[allow(dead_code)]
+//encrypt multiple 128 bit blocks using a 128 bit key
 pub fn encrypt(key: BigUint, m: BigUint) -> BigUint {
-    let slices = key.to_bytes_be();
-    let text = m.to_bytes_be();
-    //println!("Enc:{:?} {:?}", slices.len(), text.len());
-    let key = GenericArray::from_slice(&slices);
-    let block = Block::from_slice(&text);
-
-    let mut block_test = block.clone();
-    // Initialize cipher
-    let cipher = Aes128::new(&key);
-    // Encrypt block in-place
-    cipher.encrypt_block(&mut block_test);
-    let mut x = block_test.as_slice();
-
-    let num = x.read_u128::<BigEndian>().unwrap();
-    return BigUint::from(num);
-}
-
-#[allow(dead_code)]
-pub fn decrypt(key: BigUint, m: BigUint) -> BigUint {
-    let slices = key.to_bytes_be();
-    let text = m.to_bytes_be();
-    //println!("Dec:{:?} {:?}", slices.len(), text.len());
-
-    let key = GenericArray::from_slice(&slices);
-    let block = Block::from_slice(&text);
-
-    let mut block_test = block.clone();
-    // Initialize cipher
-    let cipher = Aes128::new(&key);
-    // Encrypt block in-place
-    cipher.decrypt_block(&mut block_test);
-    let mut x = block_test.as_slice();
-
-    let num = x.read_u128::<BigEndian>().unwrap();
-    return BigUint::from(num);
-}
-*/
-
-#[allow(dead_code)]
-pub fn encrypt256bytes(key: BigUint, m: BigUint) -> BigUint {
     let slices = key.to_bytes_be();
     let text = m.to_bytes_be();
     let no_of_blocks = text.len() / 16;
     assert_eq!(slices.len(), 16);
-    //assert_eq!(text.len(), 256);
     let key = GenericArray::from_slice(&slices);
 
     let mut cipher_array: Vec<u8> = vec![];
@@ -75,12 +33,12 @@ pub fn encrypt256bytes(key: BigUint, m: BigUint) -> BigUint {
 }
 
 #[allow(dead_code)]
-pub fn decrypt256bytes(key: BigUint, m: BigUint) -> BigUint {
+//decrypt multiple 128 bit blocks using a 128 bit key
+pub fn decrypt(key: BigUint, m: BigUint) -> BigUint {
     let slices = key.to_bytes_be();
     let text = m.to_bytes_be();
     let no_of_blocks = text.len() / 16;
     assert_eq!(slices.len(), 16);
-    //assert_eq!(text.len(), 256);
 
     let key = GenericArray::from_slice(&slices);
 
